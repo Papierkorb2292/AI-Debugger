7c38759a08807362bd85f39b70d4c4454ec0acf7
Papierkorb2292 <104673791+Papierkorb2292@users.noreply.github.com>
2024-06-28 08:21:24 +0200

    Added readSourceFile function

--- src/extension.ts
+++ src/extension.ts
@@ -10,6 +10,10 @@ import { DummyDebugAdapter } from './DummyDebugAdapter';
 const net = require('net');
 const cp = require('child_process');
 
+function readSourceFile(url: string): Thenable<string> {
+	return vscode.workspace.fs.readFile(vscode.Uri.parse(url)).then(buffer => buffer.toString())
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
